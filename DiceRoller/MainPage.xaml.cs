a3e61479da131ec7b22f95e73bce8412aeba0d50
MatheusLeal13 <matheusleal13@hotmail.com>
2024-03-25 22:29:50 -0300

    feat:app para calculo de dados

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using static Android.Graphics.ImageDecoder;
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+    }
+}
